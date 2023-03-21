9fcfa763320bb4f7aa38c2afb6d5b932075811b3
Thandei <67441515+Thandei@users.noreply.github.com>
2023-03-21 08:18:06 +0300

    api added

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,8 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
--- src/Controller/Admin/AuthController.php
+++ src/Controller/Admin/AuthController.php
@@ -2,29 +2,55 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route(path: '/admin/auth', name: 'app_admin_auth_')]
 class AuthController extends AbstractController
 {
     #[Route(path: '/signin', name: "signin")]
-    public function authSignin(AuthenticationUtils $authenticationUtils): Response
+    public function authSignin(AuthenticationUtils $authenticationUtils, $request): Response
     {
          if ($this->getUser()) {
               return $this->redirectToRoute('app_admin_dashboard');
          }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('admin/auth/signin.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+        // If User is not logged, create Google Client link for Sign In to Google
+        $myGoogleClient = AuthController::getGoogleAPIClient($this->getParameter("app.auth"));
+        $googleClientAuthURL = $myGoogleClient->createAuthUrl();
 
+        // If Google Sign In link has come, check token.
+        $googleClientCode = $request->get("code");
+        if ($googleClientCode) {
+            $token = $myGoogleClient->fetchAccessTokenWithAuthCode($googleClientCode);
+            if (isset($token["access_token"])) {
+                $myGoogleClient->setAccessToken($token['access_token']);
+                $googleOAuth = new Google_Service_Oauth2($myGoogleClient);
+                $googleAccountInfo = $googleOAuth->userinfo->get();
+                $loggedEmail = $googleAccountInfo->getEmail();
+                $loggedUserEntity = $userRepository->findOneBy(["email" => $loggedEmail]);
+                if ($loggedUserEntity instanceof User) {
+                    $security->login($loggedUserEntity, LoginFormAuthenticator::class);
+                    return $this->redirectToRoute(LoginFormAuthenticator::LOGIN_SUCCESS_ROUTE);
+                } else {
+                    $error = new  UserNotFoundException("No account found for this email address.", 401);
+                }
+            } else {
+                $error = new  AuthenticationException("Could not login to your Google account.", 400);
+            }
+        }
+
+        return $this->render('admin/auth/signin.html.twig', ['last_username' => $lastUsername, 'error' => $error, "googleAuthURL" => $googleClientAuthURL]);
+    }
     #[Route(path: '/cards', name: 'cards')]
     public function cards(): Response
     {
